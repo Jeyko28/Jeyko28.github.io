b7efbfd43b1b13cbce447ce26aa8dc6647845a6a
Jeyko28 <113542614+Jeyko28@users.noreply.github.com>
2024-09-06 17:01:15 -0500

    Actualizacion
    se hizo correcciones faltantes

--- js/questions.js
+++ js/questions.js
@@ -9,13 +9,13 @@
             let addPadding = question.parentElement.parentElement;
 
             addPadding.classList.toggle('questions__padding--add');
-            question.children[0].classList.toggle('questions__arrow--rotate')
+            question.children[0].classList.toggle('questions__arrow--rotate');
 
             if(answer.clientHeight === 0){
                 height = answer.scrollHeight;
             }
 
-            answer.style.height = `${height}px`
+            answer.style.height = `${height}px`;
         });
     });
 })();
